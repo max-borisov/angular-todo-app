f888de2148566ef1ee1fb9cb47b2ae15e214c2af
max-borisov <max.borisov@yahoo.com>
2015-09-30 14:05:03 +0300

    Create gh-pages

---
+++ assets/javascripts/directives.js
@@ -0,0 +1,21 @@
+(function() {
+
+  var app = angular.module('directives', []);
+
+  app.directive('projects', function() {
+    return {
+      restrict: 'E',
+      // templateUrl: '/angular-todo-app/assets/templates/projects.html'
+      templateUrl: '/assets/templates/projects.html'
+    };
+  });
+
+  app.directive('tasks', function() {
+    return {
+      restrict: 'E',
+      // templateUrl: '/angular-todo-app/assets/templates/tasks.html'
+      templateUrl: '/assets/templates/tasks.html'
+    };
+  });
+
+})();
